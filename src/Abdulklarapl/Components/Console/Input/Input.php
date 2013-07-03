88e308336dbaebde9b12fa243ae168750d1f439e
Patryk Szlagowski <szlagowskipatryk@gmail.com>
2013-07-03 23:02:53 +0200

    input object and inputInterface

--- src/Abdulklarapl/Components/Console/Input/Input.php
+++ src/Abdulklarapl/Components/Console/Input/Input.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace Abdulklarapl\Console\Input;
+namespace Abdulklarapl\Components\Console\Input;
 
 use Abdulklarapl\Components\Bag\Bag;
-use Abdulklarapl\Console\Prompt\PromptInterface;
+use Abdulklarapl\Components\Console\Prompt\PromptInterface;
 
 /**
  * Input
+ *
+ * @package Input
  *
  * @author Patryk Szlagowski <szlagowskipatryk@gmail.com>
  */
@@ -28,17 +30,42 @@ class Input implements InputInterface
 
     /**
      * @param string $msg
+     * @return string
+     */
+    public function confirm($msg = null)
+    {
+        return $this->prompt->confirm($msg);
+    }
+
+    /**
+     * @param string $msg
+     * @return string
+     */
+    public function readln($msg = null)
+    {
+        return $this->prompt->readln($msg);
+    }
+
+    /**
+     * @param string $msg
+     * @return string
      */
     public function read($msg = null)
     {
-        return new Bag($this->prompt->read($msg));
+        return $this->prompt->read($msg);
     }
 
+    /**
+     * @return Bag
+     */
     public function getParameters()
     {
         return new Bag();
     }
 
+    /**
+     * @return Bag
+     */
     public function getOptions()
     {
         return new Bag();
--- src/Abdulklarapl/Components/Console/Input/InputInterface.php
+++ src/Abdulklarapl/Components/Console/Input/InputInterface.php
@@ -1,16 +1,33 @@
 <?php
 
-namespace Abdulklarapl\Console\Input;
+namespace Abdulklarapl\Components\Console\Input;
 
 use Abdulklarapl\Components\Bag\Bag;
 
 /**
  * InputInterface
  *
+ * @package Input
+ * @subpackage Interface
+ *
  * @author Patryk Szlagowski <szlagowskipatryk@gmail.com>
  */
 interface InputInterface
 {
+
+    /**
+     * @param string $msg
+     * @return mixed
+     */
+    public function confirm($msg = null);
+
+    /**
+     * Read in new line
+     *
+     * @param string $msg
+     * @return string
+     */
+    public function readln($msg = null);
 
     /**
      * Read line
