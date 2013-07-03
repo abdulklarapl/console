7814b6ed0c3a6ad8526f8e7eb2c0a91fec228d9a
Patryk Szlagowski <szlagowskipatryk@gmail.com>
2013-07-03 23:33:05 +0200

    added eventDispatcher to console

--- src/Abdulklarapl/Components/Console/Console.php
+++ src/Abdulklarapl/Components/Console/Console.php
@@ -4,6 +4,8 @@ namespace Abdulklarapl\Components\Console;
 
 use Abdulklarapl\Components\Bag\Bag;
 use Abdulklarapl\Components\Console\Application\ConsoleApplicationInterface;
+use Abdulklarapl\Components\EventDispatcher\Dispatcher\DispatcherInterface;
+use Abdulklarapl\Components\EventDispatcher\Dispatcher\Dispatcher;
 
 /**
  * Console
@@ -19,6 +21,23 @@ class Console
      * @var Bag
      */
     private $applications;
+
+    /**
+     * @var DispatcherInterface
+     */
+    private $eventDispatcher;
+
+    /**
+     * @param DispatcherInterface $eventDispatcher
+     */
+    public function __construct(DispatcherInterface $eventDispatcher = null)
+    {
+        if (!$eventDispatcher) {
+            $eventDispatcher = new Dispatcher();
+        }
+
+        $this->eventDispatcher = $eventDispatcher;
+    }
 
     /**
      * @param ConsoleApplicationInterface $application
