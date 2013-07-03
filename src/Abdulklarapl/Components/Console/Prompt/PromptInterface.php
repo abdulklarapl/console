020d1371ef07aaf29fa9ff186c8e9dcb7b75ebe0
Patryk Szlagowski <szlagowskipatryk@gmail.com>
2013-07-03 23:03:10 +0200

    promptInterface

--- src/Abdulklarapl/Components/Console/Prompt/PromptInterface.php
+++ src/Abdulklarapl/Components/Console/Prompt/PromptInterface.php
@@ -1,11 +1,14 @@
 <?php
 
-namespace Abdulklarapl\Console\Prompt;
+namespace Abdulklarapl\Components\Console\Prompt;
 
 use Abdulklarapl\Components\Bag\Bag;
 
 /**
  * PromptInterface
+ *
+ * @package Prompt
+ * @subpackage Interface
  *
  * @author Patryk Szlagowski <szlagowskipatryk@gmail.com>
  */
@@ -13,8 +16,26 @@ interface PromptInterface
 {
 
     /**
+     * Read answer
+     *
      * @param string $msg
-     * @return array
+     * @return string
      */
     public function read($msg = null);
+
+    /**
+     * Read answer in new line
+     *
+     * @param string $msg
+     * @return string
+     */
+    public function readln($msg = null);
+
+    /**
+     * Confirm (y,n)
+     *
+     * @param string $msg
+     * @return string
+     */
+    public function confirm($msg = null);
 }
